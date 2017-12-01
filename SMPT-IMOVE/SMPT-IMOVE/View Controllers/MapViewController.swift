b14b08b022cca1dde9d2960e104ebbbbb0d0dcb1
Unknown <alekseiko@gmail.com>
2017-12-01 10:32:23 +0100

    make it display user location on mapView

---
+++ SMPT-IMOVE/SMPT-IMOVE/View Controllers/MapViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MapViewController.swift
+//  SMPT-IMOVE
+//
+//  Created by Fhict on 01/12/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //MARK: Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let manager = CLLocationManager()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestAlwaysAuthorization()
+        manager.startUpdatingLocation()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        let region = MKCoordinateRegionMakeWithDistance(location.coordinate, 5000, 5000)
+        mapView.setRegion(region, animated: true)
+        self.mapView.showsUserLocation = true
+        
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
