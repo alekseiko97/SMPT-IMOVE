1bbdb78acf1638efaa15c2bae9edb5c076c0bbee
Vogelaar,Stefan S <s.vogelaar@student.fontys.nl>
2017-12-01 13:51:03 +0100

    Weather function done

--- SMPT-IMOVE/SMPT-IMOVE/WeatherGetter.swift
+++ SMPT-IMOVE/SMPT-IMOVE/WeatherGetter.swift
@@ -87,10 +87,6 @@ struct Weather {
     let pressure: Int
     let cloudCover: Int
     let windSpeed: Double
-    
-    // These properties are optionals because OpenWeatherMap doesn't provide:
-    // - a value for wind direction when the wind speed is negligible
-    // - rain info when there is no rainfall
     let windDirection: Double?
     let rainfallInLast3Hours: Double?
     
