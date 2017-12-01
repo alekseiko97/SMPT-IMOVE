f7c550332e076e7b5adcbeee70fda5320fc918da
Shadowphoenix <github@shadowphoenix.nl>
2017-12-01 13:50:12 +0100

    Added tableviewcell classes

---
+++ SMPT-IMOVE/SMPT-IMOVE/View Controllers/TableViewCells/MapTrainingJournalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrainingJournalMapTableViewCell.swift
+//  SMPT-IMOVE
+//
+//  Created by Fhict on 01/12/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class MapTrainingJournalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SMPT-IMOVE/SMPT-IMOVE/View Controllers/TableViewCells/WeightInTrainingJournalTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeightInTrainingJournalTableViewCell.swift
+//  SMPT-IMOVE
+//
+//  Created by Fhict on 01/12/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class WeightInTrainingJournalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
