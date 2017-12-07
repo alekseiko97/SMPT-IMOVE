905c88ad147f27552b206774246ec27e30110150
Vogelaar,Stefan S <s.vogelaar@student.fontys.nl>
2017-12-07 14:42:38 +0100

    Added the table cells to the training log.

--- SMPT-IMOVE/SMPT-IMOVE/View Controllers/TrainingJournalViewController.swift
+++ SMPT-IMOVE/SMPT-IMOVE/View Controllers/TrainingJournalViewController.swift
@@ -10,56 +10,58 @@ import UIKit
 
 class TrainingJournalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    //Will be the users data from database
-    let MapTrainingTest = ["Map Test"]
-    let WorkoutTest = ["Workout Test"]
-    let WeightTest = ["Weight Test"]
-    
-    let MapTrainingCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "MapTrainingCell")
-    let WorkoutTrainingCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "WorkoutTrainingCell")
-    let WeightTrainingCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "WeightCell")
-
-    
+    @IBOutlet weak var tableview: UITableView!
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var height:CGFloat = CGFloat()
+                if indexPath.row == 1 {
+                    height = 250
+                }
+                else if indexPath.row == 2 {
+                    height = 150
+                }
+                else
+                {
+                    height = 50
+                }
+        return height
+    }
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 3
+        return(3)
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
+        let MapTrainingCell: MapTrainingJournalTableViewCell = tableView.dequeueReusableCell(withIdentifier: "MapTrainingCell", for: indexPath) as! MapTrainingJournalTableViewCell
+        let WorkoutTrainingCell: WorkoutTrainingJournalTableViewCell = tableView.dequeueReusableCell(withIdentifier: "WorkoutTableCell", for: indexPath) as! WorkoutTrainingJournalTableViewCell
+        let WeightTrainingCell: WeightInTrainingJournalTableViewCell = tableView.dequeueReusableCell(withIdentifier: "WeightInTableCell", for: indexPath) as! WeightInTrainingJournalTableViewCell
+
         if(indexPath.row == 0)
         {
-            MapTrainingCell.textLabel?.text = MapTrainingTest[0]
+            //MapTrainingCell.textLabel?.text = MapTrainingTest[0]
             return(MapTrainingCell)
         }
-        
+
         else if(indexPath.row == 1)
         {
-            WorkoutTrainingCell.textLabel?.text = WorkoutTest[0]
+            //WorkoutTrainingCell.textLabel?.text = WorkoutTest[0]
             return(WorkoutTrainingCell)
         }
         else
         {
-            WeightTrainingCell.textLabel?.text = WeightTest[0]
+            //WeightTrainingCell.textLabel?.text = WeightTest[0]
             return(WeightTrainingCell)
         }
     }
     
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if MapTrainingCell.isSelected == true
-        {
-            performSegue(withIdentifier: mapSegue, sender: <#T##Any?#>)
-        }
-        if WorkoutTrainingCell.isSelected == true
-        {
-            performSegue(withIdentifier: workoutSegue, sender: Any?)
-        }
-        if WeightTrainingCell.isSelected == true
-        {
-            performSegue(withIdentifier: weightSegue, sender: <#T##Any?#>)
-        }
+        
+        self.tableview.delegate = self
+        self.tableview.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
