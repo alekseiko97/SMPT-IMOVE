9a6dd46539e3f10269b7e17af43cf628dee76d31
Vogelaar,Stefan S <s.vogelaar@student.fontys.nl>
2017-12-07 15:02:59 +0100

    Table cells have the right height. Training journal complete.

--- SMPT-IMOVE/SMPT-IMOVE/View Controllers/TrainingJournalViewController.swift
+++ SMPT-IMOVE/SMPT-IMOVE/View Controllers/TrainingJournalViewController.swift
@@ -11,20 +11,20 @@ import UIKit
 class TrainingJournalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableview: UITableView!
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        var height:CGFloat = CGFloat()
-                if indexPath.row == 1 {
-                    height = 250
-                }
-                else if indexPath.row == 2 {
-                    height = 150
-                }
-                else
-                {
-                    height = 50
-                }
-        return height
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        var height:CGFloat = CGFloat()
+//                if indexPath.row == 1 {
+//                    height = 250
+//                }
+//                else if indexPath.row == 2 {
+//                    height = 150
+//                }
+//                else
+//                {
+//                    height = 50
+//                }
+//        return height
+//    }
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         return(3)
@@ -38,30 +38,43 @@ class TrainingJournalViewController: UIViewController, UITableViewDelegate, UITa
 
         if(indexPath.row == 0)
         {
+            self.tableview.rowHeight = 200
             //MapTrainingCell.textLabel?.text = MapTrainingTest[0]
             return(MapTrainingCell)
         }
 
         else if(indexPath.row == 1)
         {
+            self.tableview.rowHeight = 155
             //WorkoutTrainingCell.textLabel?.text = WorkoutTest[0]
             return(WorkoutTrainingCell)
         }
         else
         {
+            self.tableview.rowHeight = 44
             //WeightTrainingCell.textLabel?.text = WeightTest[0]
             return(WeightTrainingCell)
         }
     }
     
-   
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableViewAutomaticDimension
+//    }
+//
+//    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableViewAutomaticDimension
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableview.delegate = self
         self.tableview.dataSource = self
-        
+        //self.tableview.rowHeight = 200
+
+//        self.tableview.rowHeight = UITableViewAutomaticDimension
+//        self.tableview.estimatedRowHeight = 150
+//        self.tableview.reloadData()
         // Do any additional setup after loading the view.
     }
 
