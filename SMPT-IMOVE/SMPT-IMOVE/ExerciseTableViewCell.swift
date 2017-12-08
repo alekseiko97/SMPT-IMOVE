5b27a9381cc1f7cba4743c81d14d822cdba79c19
Natalya Suslova <n.suslova@student.fontys.nl>
2017-12-08 09:19:54 +0100

    some changes for events

---
+++ SMPT-IMOVE/SMPT-IMOVE/ExerciseTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ExerciseTableViewCell.swift
+//  SMPT-IMOVE
+//
+//  Created by Natalya Suslova on 08/12/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseTableViewCell: UITableViewCell {
+
+    
+
+}
