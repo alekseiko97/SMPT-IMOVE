77bf37bbe499246058f398a43acd636e219f1c98
Shadowphoenix <github@shadowphoenix.nl>
2017-12-22 09:29:30 +0100

    minor changes for natalya

---
+++ SMPT-IMOVE/SMPT-IMOVE/View Controllers/TableViewCells/WorkoutTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  WorkoutTableViewCell.swift
+//  SMPT-IMOVE
+//
+//  Created by Fhict on 22/12/2017.
+//  Copyright © 2017 Fhict. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_Exercise: UILabel!
+    @IBOutlet weak var btn_Good: UIButton!
+    @IBOutlet weak var btn_Neutral: UIButton!
+    @IBOutlet weak var btn_Bad: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        btn_Good.tintColor = UIColor(red:1.00, green:0.31, blue:0.00, alpha:1.0);
+        btn_Neutral.tintColor = UIColor(red:1.00, green:0.31, blue:0.00, alpha:1.0);
+        btn_Bad.tintColor = UIColor(red:1.00, green:0.31, blue:0.00, alpha:1.0);
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
